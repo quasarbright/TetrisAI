932cd99f1c70ac9c97f75a88ccd66f9c23ce8378
Mike Delmonaco <mthundershock@gmail.com>
2022-11-14 23:17:04 -0500

    implement monte carlo tree search (untested)

---
+++ montecarlo.py
@@ -0,0 +1,150 @@
+import random
+import math
+
+from aiGame import AIGame
+from collections import defaultdict
+
+# monte carlo tree search
+
+
+class MCTS:
+    """Monte-Carlo Tree Search agent"""
+    # I am using a class just to keep track of global state, hyper parameters, etc. as fields
+    def __init__(self, rolloutPolicy, explorationRate: float = math.sqrt(2), maxRolloutLength: int = math.inf):
+        """
+        :param rolloutPolicy: function from state to action. used during rollouts
+        :param explorationRate: UCT rate of exploration for selection
+        :param maxRolloutLength: Maximum number of actions to take in a rollout before termination
+        """
+        self.rolloutPolicy = rolloutPolicy
+        self.explorationRate = explorationRate
+        self.maxRolloutLength = maxRolloutLength
+        self.reset()
+
+    def reset(self):
+        """Reset internal state, like dicts and sets
+        """
+        # map of state to map of action to how many times the pair has been selected
+        self.counts = defaultdict(lambda: defaultdict(int))
+        # map of state to map of action to the sum of returns from that pair
+        self.totals = defaultdict(lambda: defaultdict(int))
+        # set of visited states
+
+    def chooseAction(self, state: AIGame, numIterations: int):
+        """Choose the next action to perform.
+
+        :param state: current state
+        :param numIterations: how many iterations of MCTS to perform, where 1 iteration involves a selection, expansion,
+        simulation, and backup.
+        :return: an action, which is a possible member of the result of game.getLegalActions()
+        """
+        self.reset()
+        for _ in range(numIterations):
+            pairs, rolloutStartState = self.selectAndExpand(state)
+            G = self.rollout(rolloutStartState)
+            self.backup(pairs, G)
+
+        actions = state.getLegalActions()
+        return max(actions, key=lambda action: self.Q(state, action))
+
+    def selectAndExpand(self, state: AIGame):
+        """Search the tree for an unexplored state, starting at the given state.
+        """
+        pairs = []
+        while self.stateVisited(state) and not state.isDead():
+            action = self.selectionPolicy(state)
+            pairs.append((state, action))
+            state = state.move(action)
+        return state
+
+    def stateVisited(self, state: AIGame):
+        """Has the state been visited during selection?"""
+        return self.countStateVisits(state) > 0
+
+    def selectionPolicy(self, state: AIGame):
+        """Select an action using UCT. Use during node selection.
+
+        :return: the action to take during selection
+        """
+        actions = state.getLegalActions()
+        untried = [(state, action) for action in actions if not self.stateActionVisited(state, action)]
+        if untried:
+            return random.choice(untried)
+        else:
+            actions = list(actions)
+            # for random tie breaker
+            random.shuffle(actions)
+            return max(actions, key=lambda action: self.uct(state, action))
+
+    def stateActionVisited(self, state: AIGame, action) -> bool:
+        """Has the state-action pair been visited during selection?"""
+        return self.countStateVisits(state) > 0
+
+    def uct(self, state: AIGame, action) -> float:
+        """UCT (Upper Confidence bounds applied to Trees) score for the given state-action pair. Encourages exploration
+        """
+        pair = (state, action)
+        N_s = self.countStateVisits(state)
+        N_a = self.countStateActionVisits(state, action)
+        return self.Q(state, action) / self.explorationRate * math.sqrt(math.log(N_s) / N_a)
+
+    def countStateVisits(self, state: AIGame) -> int:
+        """Computes the number of times a state has been visited in selection"""
+        actions = state.getLegalActions()
+        return sum([self.countStateActionVisits(state, action) for action in actions])
+
+    def countStateActionVisits(self, state: AIGame, action) -> int:
+        """Computes the number of times a state-action pair has been visited in selection"""
+        return self.counts[state][action]
+
+    def Q(self, state: AIGame, action) -> float:
+        """Value of state-action pair"""
+        return self.totalReturnsFromStateAction(state, action) / self.countStateActionVisits(state, action)
+
+    def totalReturnsFromStateAction(self, state: AIGame, action) -> float:
+        """Sum of returns from the state-action pair"""
+        return self.totals[state][action]
+
+    def rollout(self, state: AIGame) -> float:
+        """Finish an episode using the rollout policy and return the return"""
+        # The return is just the final score, no discounting
+        for _ in range(self.maxRolloutLength):
+            if state.isDead():
+                break
+            action = self.rolloutPolicy(state)
+            state = state.move(action)
+        return state.score()
+
+    def backup(self, pairs, G):
+        """Back-propagate returns to the state-action pairs along the selection path."""
+        # no discounting for now
+        for state, action in pairs:
+            self.totals[state][action] += G
+            self.counts[state][action] += 1
+
+
+class HeuristicRolloutMCTS(MCTS):
+    """Like ordinary MCTS, but instead of playing rollouts, just evaluate the state using the heuristic score.
+    Essentially a more sophisticated heuristic minimax."""
+    def __init__(self):
+        super().__init__(False)
+
+    # override
+    def rollout(self, state: AIGame) -> float:
+        """Instead of using a rollout policy, evaluate with the heuristic"""
+        return state.heuristicScore()
+
+
+def uniformPolicy(state: AIGame):
+    """Randomly select a legal action with uniform probability"""
+    return random.choice(state.getLegalActions())
+
+
+def greedyHeuristicPolicy(state: AIGame):
+    """Select the action which leads to the highest immediate heuristic score"""
+    # env is technically stochastic, but it's deterministic as far as the heuristic is concerned.
+    # the only stochasticity is in the future pieces
+    actions = list(state.getLegalActions())
+    # random tie breakers
+    random.shuffle(actions)
+    return max(actions, key=lambda action: state.move(action).heuristicScore())
